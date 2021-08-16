08a7f52a3a8bb8a50414bfc015ade287b5a77931
Ana Lúcia <ana.medeiros0097@gmail.com>
2021-08-16 13:22:09 -0300

    Adicionando og meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -29,8 +29,9 @@ export default function App({ Component, pageProps }) {
         />
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossOrigin="anonymous" />
         <meta property="og:title" content="Você realmente conhece Os Vingadores?" />
-        <meta property="og:description" content="Get from SEO newbie to SEO pro in 8 simple steps." />
+        <meta property="og:description" content="Faça o quiz e mostre que você sabe tudo sobre eles!" />
         <meta property="og:image" content="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fit%2Fa%2Fa6%2FThe_Avengers_Logo.png&f=1&nofb=1" />
+        <meta property="og:url" content="https://avengers-quiz" />
       </Head>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
